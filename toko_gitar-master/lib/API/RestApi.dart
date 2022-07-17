abc5fe467e6dc08fa931ea9260b20272a676da5a
Wisnuji <gibranwisnuaji@gmail.com>
2022-07-17 20:35:23 +0700

    Uas

---
+++ toko_gitar-master/lib/API/RestApi.dart
@@ -0,0 +1,18 @@
+String baseUrl = 'http://192.168.69.116:3000';
+
+//users
+String signIn = "$baseUrl/users/login";
+String signUp = "$baseUrl/users/registrasi";
+
+//barang
+String dataBarangRes = "$baseUrl/barang/get-barang";
+
+//keranjang
+String inputKeranjangRes = "$baseUrl/keranjang/input-keranjang";
+String getAllKeranjangRes = "$baseUrl/keranjang/getAll-keranjang";
+String updateKeranjangRes = "$baseUrl/keranjang/update-keranjang";
+String hapusKeranjangRes = "$baseUrl/keranjang/delete-keranjang";
+
+//transaksi
+String transaksiInput = "$baseUrl/transaksi/input";
+String getTransaksi = "$baseUrl/transaksi/getTransaksiByIdUser";
